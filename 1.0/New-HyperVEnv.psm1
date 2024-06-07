1d0e469cc4842b7136e8803a1d6d979c9f8bb4c5
Dave Goldman <39909564+dgoldman-msft@users.noreply.github.com>
2024-06-07 10:11:50 -0400

    Update New-HyperVEnv.psm1

--- 1.0/New-HyperVEnv.psm1
+++ 1.0/New-HyperVEnv.psm1
@@ -5,6 +5,3 @@ foreach ($file in (Get-ChildItem "$($script:ModuleRoot)\internal" -Filter *.ps1 
 foreach ($file in (Get-ChildItem "$($script:ModuleRoot)\functions" -Filter *.ps1 -Recurse -ErrorAction Ignore)) { . $file.FullName }
 # Import XML View Files
 foreach ($file in (Get-ChildItem "$($script:ModuleRoot)\xml" -Filter *.ps1 -Recurse -ErrorAction Ignore)) { . $file.FullName }
-
-# Load License
-. Import-Module "$($script:ModuleRoot)\internal\scripts\license.ps1"
